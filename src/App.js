704682bbf3d0e064cb573d4197c635599b048528
BalThapa <info.balthapa@gmail.com>
2023-03-28 14:42:35 +0300

    first react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Logo</h1>
       </header>
+      <div class="container">
+        <div>
+          <div class="box1">
+            <h3>Maria</h3>
+            <p>Title:CEO</p>
+            <p>Age:29</p>
+          </div>
+        </div>
+        <div>
+          <div class="box2">
+            <h3>Kati</h3>
+            <p>Title:Developer</p>
+            <p>Age:25</p>
+          </div>
+        </div>
+        <div>
+          <div class="box3">
+            <h3>Karin</h3>
+            <p>Title:Designer</p>
+            <p>Age:45</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <footer>
+          <small>copyright@2023</small>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const name = "BAL";
+const first_page = 2023;
+
+const demo = (
+  <div>
+    <h1>Hello World, {name}</h1>
+    <p>My first page was {2023 - first_page} years ago</p>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {demo}
     <App />
   </React.StrictMode>
 );
@@ -14,4 +24,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
